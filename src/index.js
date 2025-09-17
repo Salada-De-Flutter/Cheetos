cf654525e13a39aaaf8d065fbb0bc76a27622c70
Salada-De-Flutter <Fluttered001@gmail.com>
2025-09-17 12:37:35 -0300

    Primeiro ping funcionando

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// Ponto de entrada do bot Discord
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+	console.log(`Bot conectado como ${client.user.tag}`);
+});
+
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+  if (message.content === '!ping') {
+    message.reply('Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
