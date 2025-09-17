699ce04f9efaf60a38a36435d085a712576f8b7a
Salada-De-Flutter <Fluttered001@gmail.com>
2025-09-17 13:33:44 -0300

    Configurando comando de criar ficha

--- src/bot.js
+++ src/bot.js
@@ -12,6 +12,11 @@ client.on('messageCreate', message => {
   if (message.content === '!ping') {
     message.reply('Pong!');
   }
+  if (message.content === '!criarficha') {
+    const userId = message.author.id;
+    const url = `https://cheetos-gjiw.onrender.com/frontend/ficha.html?user=${userId}`;
+    message.reply(`Clique no link para preencher sua ficha: ${url}`);
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
